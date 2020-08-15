4e7c80bf9d744c601e00bc1a73851941e12d4764
Grzegorz Ciołek <ciolek.greg@gmail.com>
2020-08-15 15:00:33 +0100

    Added verbosity setting to logs.

--- src/logger.py
+++ src/logger.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import config
 
@@ -9,23 +10,38 @@ file_path = (
 def clean_file():
     global file_path
     file = open(file_path, "w")
-    if config.Debug.logging == "True":
-        file.write("------ START OF LOG FILE ----- \n")
+    if config.Logging.enable == "True":
+        file.write("------ START OF LOG FILE ------\n")
+        file.write(f"Current log verbosity: '{config.Logging.verbosity}'.\n")
+        file.write("------ STARTING GAME LOG ------\n")
     else:
         file.write("Logging is disabled.\n\n")
         file.write("Enable it in config.properties.")
     file.close()
 
 def end_log():
-    if config.Debug.logging == "True":
+    if config.Logging.enable == "True":
         global file_path
         file = open(file_path, "a")
         file.write("------ END OF LOG FILE -----")
         file.close()
 
-def log(module, output):
-    if config.Debug.logging == "True":
+def log(module, output, severity="INFO"):
+    if config.Logging.enable == "True":
+        current_time = time.strftime("%H:%M:%S")
         global file_path
         file = open(file_path, "a")
-        file.write(f"[{module}]: {output}\n")
-        file.close()
+        if config.Logging.verbosity == "DBUG":
+            file.write(f"[{current_time}][{severity}][{module}]: {output}\n")
+            file.close()
+        elif config.Logging.verbosity == "INFO" and severity != "DBUG":
+            file.write(f"[{current_time}][{severity}][{module}]: {output}\n")
+            file.close()
+        elif config.Logging.verbosity == "WARN" and severity != "DBUG" \
+            and severity != "INFO":
+            file.write(f"[{current_time}][{severity}][{module}]: {output}\n")
+            file.close()
+        elif config.Logging.verbosity == "EROR" and severity != "DBUG" \
+            and severity != "INFO" and severity != "WARN":
+            file.write(f"[{current_time}][{severity}][{module}]: {output}\n")
+            file.close()
